fbcb117a84c28d27b6ada3a91070b57d7089bf79
azizbekrakhimjonov <azizbekrahimjonov571@gmail.com>
2025-04-12 01:17:55 +0500

    setting  v2

--- root/settings.py
+++ root/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-sr)ip+jk6-e6wdk#^8m^p6o(vf)j-%xnmuxhbi6264^v)^37o=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.activity.uz', 'activity.uz']
 
 
 # Application definition
@@ -122,8 +122,8 @@ USE_TZ = False
 STATIC_URL = 'static/'
 STATIC_ROOT = '/var/www/activity.uz/static/'
 
-MEDIA_URL = 'static/'
-MEDIA_ROOT = '/var/www/activity.uz/static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = '/var/www/activity.uz/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
